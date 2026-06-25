1215fd6f5457e9c83a68e8aa80ddf4bf43d8a78a
Umesh Chandel <chandelumesh024@gmail.com>
2026-06-25 13:41:09 +0530

    update serializers with nested relations

--- codee/serializers.py
+++ codee/serializers.py
@@ -2,26 +2,41 @@ from rest_framework import serializers
 from .models import Student,School,Teacher
 
 
-class Schoolserializer(serializers.ModelSerializer):
-  class Meta:
-    model=School
-    fields='__all__'
+class SimpleSchoolSerializer(serializers.ModelSerializer):
+   class Meta:
+     model=School
+     fields=['id','name','address']
 
-
-class studentserializer(serializers.ModelSerializer):
-  school=Schoolserializer(read_only=True)
+class SimpleStudentSerailizer(serializers.ModelSerializer):
   class Meta:
     model=Student
-    fields= '__all__'
+    fields=['id','name','age']
 
-class Teacherserializer(serializers.ModelSerializer):
-  school=Schoolserializer(read_only=True)
-  student=studentserializer(many=True,read_only=True)
-  
+class SimpleTeacherSerializer(serializers.ModelSerializer):
   class Meta:
     model=Teacher
-    fields='__all__'    
+    fields=['id','name','subject']         
+   
 
+class Schoolserializer(serializers.ModelSerializer):
+  teachers=SimpleTeacherSerializer(many=True,source='teacher_set',read_only=True)
+  class Meta:
+    model=School
+    fields=['id','name','address','teacher']
+
+class studentserializer(serializers.ModelSerializer):
+  school=SimpleSchoolSerializer(read_only=True)
+  teacher=SimpleTeacherSerializer(source='teacher_set',read_only=True,many=True)
+  class Meta:
+    model=Student
+    fields=['school','id','name','age','teacher']
+
+class Teacherserializer(serializers.ModelSerializer):
+  school=SimpleSchoolSerializer(read_only=True)
+  student=SimpleStudentSerailizer(many=True,read_only=True) 
+  class Meta:
+    model=Teacher
+    fields=['school','id','name','subject','student']
     
 
   
